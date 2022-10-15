5f18ccd70a26a10b53b495aaae1e86501fb72697
medbrouk <115838852+medbrouk@users.noreply.github.com>
2022-10-15 20:08:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,17 +36,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['+212676854115'] //ur owner number
+global.ownername = "BROUK MMF" //ur owner name
+global.ytname = "YT: BOIL" //ur yt chanel name
+global.socialm = "GitHub: MEDBROUK" //ur github or insta name
+global.location = "MAROC" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['+212676854115']
+global.ownertag = '+212676854115' //ur tag number
+global.botnumber = '+212676854115' //Add your Bot Number For Log
+global.botname = 'CYBER-X BROUK' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
